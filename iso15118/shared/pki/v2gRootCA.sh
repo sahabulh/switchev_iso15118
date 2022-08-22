f6e073369338e57606c117fe2362beb35a8e577b
Md Sahabul Hossain <sahabulh@iut-dhaka.edu>
2022-08-22 12:17:10 -0600

    Added verbose to scripts

--- iso15118/shared/pki/v2gRootCA.sh
+++ iso15118/shared/pki/v2gRootCA.sh
@@ -106,7 +106,7 @@ fi
 
 echo "Password used is: '$password'"
 
-# 0) Create directories if not yet existing
+# Create directories if not yet existing
 CERT_PATH=$ISO_FOLDER/certs
 KEY_PATH=$ISO_FOLDER/private_keys
 CSR_PATH=$ISO_FOLDER/csrs
@@ -115,49 +115,24 @@ mkdir -p $CSR_PATH
 mkdir -p $KEY_PATH
 
 
-# 1) Create a self-signed V2GRootCA certificate
-# ---------------------------------------------
-# 1.1) Create a
-#	- private key -> -genkey
-#	- with elliptic curve parameters -> ecparam
-#	- using the chosen named elliptic curve $EC_CURVE -> -name $EC_CURVE
-#	- encrypt the key with chosen symmetric cipher $SYMMETRIC_CIPHER using the
-#	  'ec' utility command -> ec $SYMMETRIC_CIPHER
-# - the passphrase for the encryption of the private key is provided in a file
-#   -> -passout pass:$password
-#	- save the encrypted private key at the location provided -> -out
+# Create a self-signed V2GRootCA certificate
+
+# Generate private key
 if [ $version == $ISO_2 ];
 then
     openssl ecparam -genkey -name $EC_CURVE | openssl ec $SYMMETRIC_CIPHER -passout pass:$password -out $KEY_PATH/v2gRootCA.key
 else
     openssl genpkey -algorithm $EC_CURVE $SYMMETRIC_CIPHER -pass pass:$password -out $KEY_PATH/v2gRootCA.key
 fi
-# 1.2) Create a certificate signing request (CSR)
-#	- new -> -new
-#	- certificate signing request -> req
-# - using the previously created private key from which the public key can be
-#   derived -> -key
-#	- use the password to decrypt the private key -> -passin
-#	- take the values needed for the Distinguished Name (DN) from the
-#	  configuration file -> -config
-#	- save the CSR at the location provided -> -out
-openssl req -new -key $KEY_PATH/v2gRootCA.key -passin pass:$password -config configs/v2gRootCACert.cnf -out $CSR_PATH/v2gRootCA.csr
-# 1.3) Create an X.509 certificate
-#	- use the X.509 utility command -> x509
-#	- requesting a new X.509 certificate -> -req
-#	- using a CSR file that is located at -> -in
-#	- we need an X.509v3 (version 3) certificate that allows for extensions.
-#	  Those are specified in an extensions file -> -extfile
-#	- that contains a section marked with 'ext' -> -extensions
-# - self-sign the certificate with the previously generated private key -> -signkey
-#	- use the password to decrypt the private key -> -passin
-#	- tell OpenSSL to use the chosen hash algorithm $SHA for creating the digital
-#	  signature (otherwise SHA1 would be used) -> $SHA
-#	- each issued certificate must contain a unique serial number assigned by the CA (must be unique within the issuers number range) -> -set_serial
-#	- save the certificate at the location provided -> -out
-# 	- make the certificate valid for 40 years (give in days) -> -days
-openssl x509 -req -in $CSR_PATH/v2gRootCA.csr -extfile configs/v2gRootCACert.cnf -extensions ext -signkey $KEY_PATH/v2gRootCA.key -passin pass:$password -set_serial 12345 -out $CERT_PATH/v2gRootCACert.pem -days $VALIDITY_V2G_ROOT_CERT
 
-# 16) Finally we need to convert the certificates from PEM format to DER format
-#     (PEM is the default format, but ISO 15118 only allows DER format)
+# Create a CSR
+openssl req -new -key $KEY_PATH/v2gRootCA.key -passin pass:$password -config configs/v2gRootCACert.cnf -out $CSR_PATH/v2gRootCA.csr
+echo "CSR generation is done."
+
+# Create an X.509 certificate 
+openssl x509 -req -in $CSR_PATH/v2gRootCA.csr -extfile configs/v2gRootCACert.cnf -extensions ext -signkey $KEY_PATH/v2gRootCA.key -passin pass:$password -set_serial 12345 -out $CERT_PATH/v2gRootCACert.pem -days $VALIDITY_V2G_ROOT_CERT
+echo "Certificate generation and signing is finished."
+
+# Convert the certificates from PEM format to DER format
 openssl x509 -inform PEM -in $CERT_PATH/v2gRootCACert.pem -outform DER -out $CERT_PATH/v2gRootCACert.der
+echo "Certificate has been converted ans saved in DER format."
