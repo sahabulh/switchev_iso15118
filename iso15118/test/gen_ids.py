d94362fea3acb6e2e5564ff4cc60ab3aa2c6fd3f
Md Sahabul Hossain <sahabulh@iut-dhaka.edu>
2023-12-11 13:55:51 -0700

    Revert "Certificate isuance added"
    This reverts commit 1cf4ecadcde38a7dc79296dd4299595be77592b8.

--- iso15118/test/gen_ids.py
+++ iso15118/test/gen_ids.py
@@ -1,50 +1,50 @@
-def get_generator(pad, offset):
-    num = 1 + offset
+def get_generator(pad):
+    num = 1
     while True:
         out = format(num, 'x').upper()
         yield out.zfill(pad)
         num += 1
-
-def get_EMAID(offset = 0):
+        
+def get_EMAID():
     # Used in the contract certificate (ISO 15118-20 C.1 and ISO 15118-2 H.1)
     # For now, country code 'US', Provider ID 'EMA' and ID type 'C', are hard coded
     # eMA Instance: at least 8 character
-    gen = get_generator(8, offset)
+    gen = get_generator(8)
     while True:
         yield 'USEMAC'+next(gen)
-
-def get_PCID(offset = 0):
+        
+def get_PCID():
     # Used in the OEM provisioning certificate (ISO 15118-20 C.2)
     # For now, WMI code '1EV', ID type 'P', are hard coded
     # OEM's own unique ID: at least 13 character
-    gen = get_generator(13, offset)
+    gen = get_generator(13)
     while True:
         yield '1EVP'+next(gen)
-
-def get_SECCID(offset = 0):
+        
+def get_SECCID():
     # Used in the SECC certificate (ISO 15118-20 C.3)
     # For now, country code 'US', EVSE Operator ID '2SE' and ID type 'S', are hard coded
     # Controller ID: at least 32 character
-    gen = get_generator(32, offset)
+    gen = get_generator(32)
     while True:
         yield 'US2SES'+next(gen)
-
-def get_EVSEID(offset = 0):
+        
+def get_EVSEID():
     # Used for EVSE identification (ISO 15118-20 C.4 and ISO 15118-2 H.2)
     # For now, country code 'US', EVSE Operator ID '2SE' and ID type 'E', are hard coded
     # Power Outlet ID: 1 to 31 character(s). We will use 9 characters. 
-    gen = get_generator(9, offset)
+    gen = get_generator(9)
     while True:
         yield 'US2SEE'+next(gen)
-
-def get_EVCCID(offset = 0):
+        
+def get_EVCCID():
     # Used in the EVCC or EV device certificate (ISO 15118-20 C.5)
     # For now, WMI code '1EV', ID type 'V', are hard coded
     # Power Outlet ID: 1 to 31 character(s). We will use 9 characters. 
-    gen = get_generator(9, offset)
+    gen = get_generator(9)
     while True:
         yield '1EVV'+next(gen)
-
+        
 def example(id_type, num):
     # Usage: example('emaid',10)
     if id_type == 'emaid':
